61185f179dc46b4128f7bd3c939a653f11480b52
M <m@mathsistor.com>
2024-05-07 15:54:37 -0500

    Create service table

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 # Enum for Appointment status
 from datetime import datetime
+from decimal import Decimal
 from enum import Enum
 
 from pydantic import EmailStr
-from sqlmodel import AutoString, Column, Field, Relationship, SQLModel
+from sqlmodel import AutoString, Column, Field, SQLModel, DECIMAL, Relationship
+
 
 from .core.models import BaseModel
 
@@ -13,30 +15,38 @@ class AppointmentStatus(str, Enum):
     completed = "completed"
     canceled = "canceled"
 
+
 class MedSpa(BaseModel, table=True):
     """Model representing a medical spa center."""
+
     name: str
     address: str
     phone_number: str
     email: EmailStr = Field(sa_type=AutoString, unique=True, nullable=False)
 
-#     # Relationship with services and appointments
-#     services: list["Service"] = Relationship(back_populates="med_spa")
+    #     # Relationship with services and appointments
+    services: list["Service"] = Relationship(back_populates="med_spa")
+
+
 #     appointments: list["Appointment"] = Relationship(back_populates="med_spa")
 
 
-# class Service(BaseModel, table=True):
-#     """Model representing a service offered by a MedSpa."""
-#     name: str
-#     description: str
-#     price: float = Field(sa_column=Column("price", nullable=False))
-#     duration: int  # Duration in minutes
+class Service(BaseModel, table=True):
+    """Model representing a service offered by a MedSpa."""
 
-#     med_spa_id: int = Field(foreign_key="med_spa.id")
-#     med_spa: MedSpa = Relationship(back_populates="services")
+    name: str
+    description: str
+    price: Decimal = Field(
+        default=None, sa_column=Column(DECIMAL(precision=10, scale=2), nullable=False)
+    )
+    duration: int  # Duration in minutes
 
-#     # Relationship to appointments through a secondary table
-#     appointments: list["Appointment"] = Relationship(back_populates="services", link_model="AppointmentService")
+    med_spa_id: int = Field(foreign_key="med_spa.id")
+    med_spa: MedSpa = Relationship(back_populates="services")
+
+    # Relationship to appointments through a secondary table
+    # appointments: list["Appointment"] = Relationship(back_populates="services", link_model="AppointmentService")
+
 
 # class Appointment(BaseModel, table=True):
 #     """Model representing an appointment at a MedSpa."""
