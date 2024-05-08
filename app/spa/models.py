d3a4ac9c3e4361a3848790e25b66fc937641756d
M <m@mathsistor.com>
2024-05-08 07:28:16 -0500

    Remove needless logic

--- app/spa/models.py
+++ app/spa/models.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 from decimal import Decimal
 from enum import StrEnum, auto
-from typing import Self
 
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import EmailStr
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import DECIMAL, AutoString, Column, Field, Relationship, SQLModel
 
 from ..core.models import BaseModel
@@ -74,41 +72,8 @@ class Appointment(BaseModel, table=True):
         back_populates="appointments", link_model=AppointmentServiceLink
     )
 
-    # async def total_duration(self, session: AsyncSession) -> int:
-    #     """Calculate total duration from all associated services."""
-    #     await session.refresh(self)
-    #     services = await self.services
-    #     return sum(service.duration for service in services if service.duration)
-
-    # async def total_price(self, session: AsyncSession) -> Decimal:
-    #     """Calculate total price from all associated services."""
-    #     await session.refresh(self)
-    #     services = await self.services
-    #     return sum(service.price for service in services if service.price)
-
 
 class AppointmentCreate(PydanticBaseModel):
     med_spa_id: int
     start_time: datetime
     service_ids: list[int]
-
-
-# class AppointmentResponse(PydanticBaseModel):
-#     start_time: datetime
-#     status: AppointmentStatus
-#     med_spa_id: int
-#     total_duration: int
-#     total_price: Decimal
-
-#     @staticmethod
-#     async def from_orm(model: Appointment, session: AsyncSession) -> Self:
-#         """Convert SQLModel instance to Pydantic model including computed properties."""
-#         total_duration = await model.total_duration(session)
-#         total_price = await model.total_price(session)
-#         return AppointmentResponse(
-#             start_time=model.start_time,
-#             status=model.status,
-#             med_spa_id=model.med_spa_id,
-#             total_duration=total_duration,
-#             total_price=total_price
-#         )
